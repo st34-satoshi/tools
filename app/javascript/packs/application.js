bf0b5d70c2b34997950b4451c86f33e943daac0c
satoshi-tanaka <st34.satoshi@gmail.com>
2022-08-14 11:05:39 +0900

    update: able to use javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,5 +12,7 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-//= require jquery/dist/jquery.js
+//= require jquery/dist/jquery
 //= require bootstrap/dist/js/bootstrap.js
+
+// require('jquery')
