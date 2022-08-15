2ca5bfdca3a88a83378c7f2edc1c4dc09bec019b
Satoshi Tanaka <st34.satoshi@gmail.com>
2022-08-15 23:14:27 +0900

    update: able to enter answer

--- app/javascript/packs/pi-index.js
+++ app/javascript/packs/pi-index.js
@@ -1,13 +1,41 @@
 // javascript for remember pi
 
 let ANSWER = '';
+let CURRENT_DIGIT = 0;
 
 function setAnswer(){
     const ans = $('#pi-answer').text().replace(/[^0-9]/g, ''); // remove non-numeric characters
     ANSWER = ans;
 }
 
+function updateEnteredAnswer(n){
+    // check if the answer is correct or not.
+    if(ANSWER[CURRENT_DIGIT] === String(n)){
+        // correct answer
+        const current = $('#entered-answer').text();
+        $('#entered-answer').text(current + n);
+
+        CURRENT_DIGIT += 1;
+        $('#current-digit').text(CURRENT_DIGIT);
+    }else{
+        // wrong answer
+        // TODO: wrong sound
+        console.log('wrong');
+    }
+}
+
+function setButtons(){
+    // number button
+    for(let i=0;i<=9;i++){
+        $(`#button-${i}`).on('click', function() {
+            console.log('button is clicked' + i);
+            updateEnteredAnswer(i);
+        });
+    }
+}
+
 $(function() {
-    console.log('hello js');
+    console.log('hello js i');
     setAnswer();
+    setButtons();
 });
