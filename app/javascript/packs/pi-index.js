3d223499e1c5844b1c3d568306b4b22e0e2b9dff
Satoshi Tanaka <st34.satoshi@gmail.com>
2022-08-15 22:56:40 +0900

    update: set pi answer

--- app/javascript/packs/pi-index.js
+++ app/javascript/packs/pi-index.js
@@ -1,3 +1,13 @@
+// javascript for remember pi
+
+let ANSWER = '';
+
+function setAnswer(){
+    const ans = $('#pi-answer').text().replace(/[^0-9]/g, ''); // remove non-numeric characters
+    ANSWER = ans;
+}
+
 $(function() {
     console.log('hello js');
+    setAnswer();
 });
