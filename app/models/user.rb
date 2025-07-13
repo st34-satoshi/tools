7b1b30d83b2dce5d2722a9a8589396a331267977
Satoshi Tanaka <st34.satoshi@gmail.com>
2025-07-13 15:49:58 +0900

    save image within transaction

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ApplicationRecord
+  has_one_attached :avatar
+
+  def save_image
+    avatar.attach(io: File.open("app/assets/images/remember-pi/pi.png"),filename: "avatar.png")
+    save
+  end
+
+  def self.save_images
+    transaction do
+      User.all.each do |user|
+        user.save_image
+      end
+    end
+  end
+
+  def self.urls
+    User.all.map do |user|
+      user.avatar_url
+    end
+  end
+
+  def avatar_url
+    if avatar.attached?
+      Rails.application.routes.url_helpers.rails_blob_path(avatar, only_path: true)
+    end
+  end
+end
